b2c9caecb5945097a9d5bf604fe5297eb2d21496
Philip James <pjj@philipjohnjames.com>
2015-09-23 10:10:34 -0700

    Use iscool over wordfilter

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ var _ = require('underscore');
 _.mixin( require('underscore.deferred') );
 var Twit = require('twit');
 var T = new Twit(require('./config.js'));
-var wordfilter = require('wordfilter');
+var createIsCool = require('iscool');
 var ent = require('ent');
 var wordnikKey = require('./permissions.js').key;
 
 var getNounUrl = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=noun-plural,noun-posessive,proper-noun,proper-noun-plural,proper-noun-posessive&minCorpusCount=100&minDictionaryCount=13&minLength=3&maxLength=8&api_key=' + wordnikKey;
 
+var isCool = createIsCool();
+
 Array.prototype.pick = function() {
   return this[Math.floor(Math.random()*this.length)];
 };
 
-fruits = ['berries', 'fruit'];
+// fruits = ['berries',];
 
 function tweetWord(word) {
     T.post('statuses/update', { status: word }, function(err, reply) {
@@ -37,9 +39,9 @@ function tweetSnozz() {
         if (!error) {
             noun = nounData.word;
             // if we got back some data from the API and it didn't error
-            if (!wordfilter.blacklisted(noun) && !(noun[0] === noun[0].toUpperCase())){
+            if (isCool(noun) && !(noun[0] === noun[0].toUpperCase())){
                 console.log(noun);
-                fruitstring = noun + fruits.pick();
+                fruitstring = noun + 'berries';
                 tweet += fruitstring + ' taste like ' + fruitstring +'!';
                 console.log(tweet);
                 tweetWord(tweet);
